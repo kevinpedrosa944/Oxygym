8e3823cab815145dcf409d06c79e0d1e6dfff946
OXYGEN143NOTAVAILABLE <segoviajairus339@gmail.com>
2025-11-17 02:05:20 +0800

    z

--- logout.php
+++ logout.php
@@ -8,6 +8,12 @@ foreach ($_COOKIE as $key => $value) {
     setcookie($key, '', time() - 3600, '/');
 }
 
+// Prevent browser caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: 0");
+
 // Redirect to home
 header("Location: /Oxygym/index.html");
 exit();
