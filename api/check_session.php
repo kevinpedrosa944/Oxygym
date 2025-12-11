a571ac5b75d7bb13d7439c3bf0ab3f4990b324d8
OXYGEN143NOTAVAILABLE <segoviajairus339@gmail.com>
2025-12-12 02:57:49 +0800

    d

--- api/check_session.php
+++ api/check_session.php
@@ -3,6 +3,7 @@ session_start();
 header('Content-Type: application/json');
 
 echo json_encode([
+    'loggedIn' => isset($_SESSION['username']),
     'authenticated' => isset($_SESSION['username']),
     'username' => $_SESSION['username'] ?? null,
     'role' => $_SESSION['role'] ?? null
